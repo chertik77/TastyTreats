eeae5397aafb6a2309a9f1bc05422e6523d0ea73
chertik77 <babychdenya@gmail.com>
2024-01-11 13:28:20 +0000

    feat: add hook-form-persist

--- src/hooks/userOrderForm.ts
+++ src/hooks/userOrderForm.ts
@@ -1,5 +1,6 @@
 import { valibotResolver } from '@hookform/resolvers/valibot'
 import { useForm } from 'react-hook-form'
+import useFormPersist from 'react-hook-form-persist'
 import { orderSchema, type Data } from 'utils/schema'
 
 export const useOrderForm = () => {
@@ -14,11 +15,17 @@ export const useOrderForm = () => {
   } = useForm<Data>({
     resolver: valibotResolver(orderSchema),
     mode: 'onChange',
-    defaultValues: { phone: '' },
     progressive: true
   })
 
-  // useFormPersist('order-now-form', { watch, setValue, storage: window.localStorage })
+  const isWindowDefined = () => {
+    if (typeof window !== 'undefined') {
+      return window.localStorage
+    }
+    return undefined
+  }
+
+  useFormPersist('order-now-form', { watch, setValue, storage: isWindowDefined() })
 
   return { register, handleSubmit, control, reset, errors, isValid }
 }
