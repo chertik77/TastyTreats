719cc94042799c8d835da62b98bad220c1da97ca
chertik77 <babychdenya@gmail.com>
2024-01-12 01:59:16 +0000

    refactor: update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
 import { Hero } from 'components/blocks/Hero'
 import { Categories } from 'components/blocks/categories/Categories'
 import { PopularRecipes } from 'components/blocks/popular-recipes/PopularRecipes'
-import { RecipeModal } from 'components/ui/RecipeModal'
-import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
-import { parseAsInteger } from 'nuqs/parsers'
 
-// type SearchParamProps = {
-//   searchParams: Record<string, string> | null | undefined
-// }
-
-const counterParser = parseAsInteger.withDefault(1)
-
-export default function Home({ searchParams }: Params) {
-  console.log(searchParams)
+export default function Home() {
   return (
     <>
       <Hero />
       <Categories />
       <PopularRecipes />
-      {searchParams.id && <RecipeModal />}
     </>
   )
 }
