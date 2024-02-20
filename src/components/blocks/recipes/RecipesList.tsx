01e7fed8cfada29afeeb6a35b209fa2c4fd075b5
Denys <babychdenya@gmail.com>
2024-02-20 21:05:41 +0000

    refactor: remove recipes list animation

--- src/components/blocks/recipes/RecipesList.tsx
+++ src/components/blocks/recipes/RecipesList.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Rating } from 'components/ui/Rating'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import type { Recipes } from 'types/recipes.types'
 
 type RecipesListProps = {
@@ -11,88 +11,80 @@ type RecipesListProps = {
 
 export const RecipesList = ({ filterQuery, recipes }: RecipesListProps) => {
   return (
-    <AnimatePresence>
-      <motion.ul
-        initial={{ opacity: 0.8, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0 }}
-        transition={{ duration: 0.5 }}>
-        <ul className='flex flex-col gap-6 tablet:flex-row tablet:flex-wrap tablet:gap-4'>
-          {filterQuery
-            ? recipes.results
-                .filter(({ title }) =>
-                  title.toLowerCase().includes(filterQuery.toLowerCase())
-                )
-                .map(({ _id, description, preview, rating, title }) => (
-                  <motion.li
-                    className='relative size-[335px] rounded-lg px-4 pb-4 pt-[214px] desktop:h-[287px]
+    <ul className='flex flex-col gap-6 tablet:flex-row tablet:flex-wrap tablet:gap-4'>
+      {filterQuery
+        ? recipes.results
+            .filter(({ title }) =>
+              title.toLowerCase().includes(filterQuery.toLowerCase())
+            )
+            .map(({ _id, description, preview, rating, title }) => (
+              <motion.li
+                className='relative size-[335px] rounded-lg px-4 pb-4 pt-[214px] desktop:h-[287px]
                       desktop:w-[250px] desktop:pt-[163px] tablet:h-[264px] tablet:w-[240px]
                       tablet:pt-[143px]'
-                    key={_id}
-                    style={{
-                      background: `linear-gradient(1deg, rgba(5, 5, 5, 0.60) 4.82%, rgba(5, 5, 5, 0.00) 108.72%), url(${preview}), lightgray -91.79px -9.338px / 138.4% 120.558% no-repeat`
-                    }}>
-                    <button className='absolute right-4 top-4' type='button'>
-                      <i className='pi pi-heart text-[22px] text-lighter opacity-50 dark:text-light'></i>
-                    </button>
-                    <div>
-                      <h3 className='mb-2 truncate text-fs-14-lh-128-fw-600 text-light'>
-                        {title}
-                      </h3>
-                      <p className='line-clamp-2 text-fs-12-lh-133-fw-400 text-light'>
-                        {description}
-                      </p>
-                    </div>
-                    <div className='mt-4 flex justify-between'>
-                      <div className='flex items-center gap-1'>
-                        <p className='mb-[-3px] text-fs-14-lh-normal-fw-500 text-light'>
-                          {rating.toFixed(1)}
-                        </p>
-                        <Rating rating={rating} />
-                      </div>
-                      <button className='btn-recipe' type='button'>
-                        See recipe
-                      </button>
-                    </div>
-                  </motion.li>
-                ))
-            : recipes.results.map(
-                ({ _id, description, preview, rating, title }) => (
-                  <li
-                    className='relative size-[335px] rounded-lg px-4 pb-4 pt-[214px] desktop:h-[287px]
+                key={_id}
+                style={{
+                  background: `linear-gradient(1deg, rgba(5, 5, 5, 0.60) 4.82%, rgba(5, 5, 5, 0.00) 108.72%), url(${preview}), lightgray -91.79px -9.338px / 138.4% 120.558% no-repeat`
+                }}>
+                <button className='absolute right-4 top-4' type='button'>
+                  <i className='pi pi-heart text-[22px] text-lighter opacity-50 dark:text-light'></i>
+                </button>
+                <div>
+                  <h3 className='mb-2 truncate text-fs-14-lh-128-fw-600 text-light'>
+                    {title}
+                  </h3>
+                  <p className='line-clamp-2 text-fs-12-lh-133-fw-400 text-light'>
+                    {description}
+                  </p>
+                </div>
+                <div className='mt-4 flex justify-between'>
+                  <div className='flex items-center gap-1'>
+                    <p className='mb-[-3px] text-fs-14-lh-normal-fw-500 text-light'>
+                      {rating.toFixed(1)}
+                    </p>
+                    <Rating rating={rating} />
+                  </div>
+                  <button className='btn-recipe' type='button'>
+                    See recipe
+                  </button>
+                </div>
+              </motion.li>
+            ))
+        : recipes.results.map(
+            ({ _id, description, preview, rating, title }) => (
+              <li
+                className='relative size-[335px] rounded-lg px-4 pb-4 pt-[214px] desktop:h-[287px]
                       desktop:w-[250px] desktop:pt-[163px] tablet:h-[264px] tablet:w-[240px]
                       tablet:pt-[143px]'
-                    key={_id}
-                    style={{
-                      background: `linear-gradient(1deg, rgba(5, 5, 5, 0.60) 4.82%, rgba(5, 5, 5, 0.00) 108.72%), url(${preview}), lightgray -91.79px -9.338px / 138.4% 120.558% no-repeat`
-                    }}>
-                    <button className='absolute right-4 top-4' type='button'>
-                      <i className='pi pi-heart text-[22px] text-lighter opacity-50 dark:text-light'></i>
-                    </button>
-                    <div>
-                      <h3 className='mb-2 truncate text-fs-14-lh-128-fw-600 text-light'>
-                        {title}
-                      </h3>
-                      <p className='line-clamp-2 text-fs-12-lh-133-fw-400 text-light'>
-                        {description}
-                      </p>
-                    </div>
-                    <div className='mt-4 flex justify-between'>
-                      <div className='flex items-center gap-1'>
-                        <p className='mb-[-3px] text-fs-14-lh-normal-fw-500 text-light'>
-                          {rating.toFixed(1)}
-                        </p>
-                        <Rating rating={rating} />
-                      </div>
-                      <button className='btn-recipe' type='button'>
-                        See recipe
-                      </button>
-                    </div>
-                  </li>
-                )
-              )}
-        </ul>
-      </motion.ul>
-    </AnimatePresence>
+                key={_id}
+                style={{
+                  background: `linear-gradient(1deg, rgba(5, 5, 5, 0.60) 4.82%, rgba(5, 5, 5, 0.00) 108.72%), url(${preview}), lightgray -91.79px -9.338px / 138.4% 120.558% no-repeat`
+                }}>
+                <button className='absolute right-4 top-4' type='button'>
+                  <i className='pi pi-heart text-[22px] text-lighter opacity-50 dark:text-light'></i>
+                </button>
+                <div>
+                  <h3 className='mb-2 truncate text-fs-14-lh-128-fw-600 text-light'>
+                    {title}
+                  </h3>
+                  <p className='line-clamp-2 text-fs-12-lh-133-fw-400 text-light'>
+                    {description}
+                  </p>
+                </div>
+                <div className='mt-4 flex justify-between'>
+                  <div className='flex items-center gap-1'>
+                    <p className='mb-[-3px] text-fs-14-lh-normal-fw-500 text-light'>
+                      {rating.toFixed(1)}
+                    </p>
+                    <Rating rating={rating} />
+                  </div>
+                  <button className='btn-recipe' type='button'>
+                    See recipe
+                  </button>
+                </div>
+              </li>
+            )
+          )}
+    </ul>
   )
 }
